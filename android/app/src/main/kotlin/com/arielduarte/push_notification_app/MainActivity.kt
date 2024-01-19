c4514230211fd2d67c93f4ba3f348ea0d746aada
ariel.duarte <arielpy@gmail.com>
2024-01-18 23:51:53 -0300

    Added FlutterFire configuration

--- android/app/src/main/kotlin/com/arielduarte/push_notification_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/arielduarte/push_notification_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.push_notification_app
+package com.arielduarte.push_notification_app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:push_notification_app/presentation/blocs/notifications/notifications_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -7,13 +9,16 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: const _HomeView(),
-      appBar: AppBar(title: const Text('Push Notification App'), actions: [
-        IconButton(
-            onPressed: () {
-              // Solicitar permisos de notificaciones
-            },
-            icon: const Icon(Icons.settings))
-      ]),
+      appBar: AppBar(
+          title: context
+              .select((NotificationsBloc bloc) => Text('${bloc.state.status}')),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  context.read<NotificationsBloc>().requestPermission();
+                },
+                icon: const Icon(Icons.settings))
+          ]),
     );
   }
 }
